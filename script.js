1baa0f0b839d264cb0fa6b7a2deb4ec073bdf5c8
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-05-11 19:36:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+//ARITHMETIC OPERATORS
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = " , a , "b = " , b);
+
+// console.log("a + b = " + (a + b)); // Addition
+
+// console.log("a - b = " + (a - b)); // Subtraction
+
+// console.log("a * b = " + (a * b)); // Multiplication
+
+// console.log("a / b = " + (a / b)); // Division
+
+// console.log("a % b = " + (a % b)); // Modulus
+
+// console.log("a ** b = " + (a ** b)); // Exponentiation
+
+
+
+//UNARY OPERATORS
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a = " , a , "b = " , b);
+
+// a++; // a=a+1
+// console.log("a = " , a);
+
+// a--; // a=a-1
+// console.log("a = " , a);
+
+
+
+//ASSIGNMENT OPERATORS
+
+// let a = 5;
+// let b = 2;
+
+// a+=4; // a = a + 4
+// console.log("a = " , a); 
+
+// a-=3; // a = a - 3
+// console.log("a = " , a);
+
+// a*=2; // a = a * 2
+// console.log("a = " , a);
+
+// a/=2; // a = a / 2  
+// console.log("a = " , a);
+
+// a%=3; // a = a % 3
+// console.log("a = " , a);
+
+// a**=3; // a = a ** 3
+// console.log("a = " , a);
+
+
+
+//COMPARISON OPERATORS
+
+// let a = 5;
+// let b = 2;
+// let c = 5;
+// let d = "5";
+
+// console.log("5 == 2 : " , a == b); 
+// console.log("5 == 5 : " , a == c); 
+// console.log("5 == str(5) : " , a == d);
+
+// console.log("5 != 5 : " , a != c);
+// console.log("5 != 2 : " , a != b); 
+
+// console.log("5 === str(5) : " , a === d); // Strict Equality
+
+// console.log("5 !== str(5) : " , a !== d); // Strict Inequality
+
+// console.log("5 > 2 : " , a > b);
+
+// console.log("5 < 2 : " , a < b);
+
+// console.log("5 >= 5 : " , a >= c);
+
+// console.log("5 <= 2 : " , a <= b);
+
+// console.log("5 <= 5 : " , a <= c);
+
+
+
+//LOGICAL OPERATORS
+
+let a = 6;
+let b = 5;
+
+let cond1 = a > b; // true
+let cond2 = a === b; // false
+let cond3 = a < b; // false
+
+console.log("cond1 AND cond2 : " , cond1 && cond2); 
+
+console.log("cond1 AND cond3 : " , cond1 && cond3);
+ 
+console.log("cond1 OR cond2 : " , cond1 || cond2); 
