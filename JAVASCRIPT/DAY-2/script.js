b0a620d86b73658d2f5ac7cbf584074c2e92e3cb
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-04 23:29:32 +0530

    JS

---
+++ JAVASCRIPT/DAY-2/script.js
@@ -0,0 +1,35 @@
+// Arithmetic Operators
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a , "b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+
+
+// Unary Operators
+let a = 5;
+let b = 2;
+
+console.log("a = ", a , "b = ", b);
+
+// a++;       // a = a + 1
+// console.log("a = ", a);
+
+// a--;       // a = a - 1
+// console.log("a = ", a);
+
+// console.log("++a = ", ++a);     //6
+
+// console.log("a++ = ", a++);   //5
+// console.log("a = ", a);       //6
+
+// console.log("--a = ", --a);     //4
+
+console.log("a++ = ", a++);   //5
+console.log("a = ", a);       //4
