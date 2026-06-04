85ccd7f636d5427a28e5c61f61b72db6f7a13f45
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-04 23:55:08 +0530

    JS

--- JAVASCRIPT/DAY-2/script.js
+++ JAVASCRIPT/DAY-2/script.js
@@ -13,10 +13,10 @@
 
 
 // Unary Operators
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
-console.log("a = ", a , "b = ", b);
+// console.log("a = ", a , "b = ", b);
 
 // a++;       // a = a + 1
 // console.log("a = ", a);
@@ -31,5 +31,45 @@ console.log("a = ", a , "b = ", b);
 
 // console.log("--a = ", --a);     //4
 
-console.log("a++ = ", a++);   //5
-console.log("a = ", a);       //4
+// console.log("a++ = ", a++);   //5
+// console.log("a = ", a);       //4
+
+
+
+// Assignment Operators
+// let a = 5;
+// let b = 2;
+
+// a += 4;   //a = a + 4
+// console.log("a = ", a); 
+
+// a -= 4;   //a = a - 4
+// console.log("a = ", a);
+
+// a *= 4;   //a = a * 4
+// console.log("a = ", a);
+
+// a /= 4;   //a = a / 4
+// console.log("a = ", a);
+
+// a %= 4;   //a = a % 4
+// console.log("a = ", a);
+
+// a **= 4;   //a = a ** 4
+// console.log("a = ", a);
+
+
+
+// Comparison Operators
+let a = 5;
+let b = 2;
+let c = 5;
+let d = 
+
+console.log("5 == 2: ", a == b)
+
+console.log("5 != 2: ", a != b)
+
+console.log("5 == 5: ", a == c)
+
+console.log("5 != 5: ", a != c)
