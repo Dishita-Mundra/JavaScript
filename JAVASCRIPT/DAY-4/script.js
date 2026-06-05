f547680065f0753b77922df2f859b6e5c9cd624e
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-05 18:22:36 +0530

    JS

--- JAVASCRIPT/DAY-4/script.js
+++ JAVASCRIPT/DAY-4/script.js
@@ -48,3 +48,40 @@
 // }
 
 
+// PRACTICE
+
+// Q1           
+// let marks = [85, 97, 44, 37, 76, 60];
+
+// let sum = 0;
+
+// for (let val of marks) {
+//     sum += val;
+// }
+
+// let avg = sum / marks.length
+
+// console.log(`avg marks of the class is ${avg}`);
+
+
+// Q2
+let items = [250, 645, 300, 900, 50];
+
+// let i = 0;
+
+// for (let val of items) {
+
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+
+//     console.log(`value after offer = ${items[i]}`)
+//     i++;
+// }
+
+// for (let i = 0; i < items.length; i++) {
+//     let offer = items[i] / 10;
+
+//     items[i] -= offer;
+// }
+
+// console.log(items);
