a43e8c2130ea5603b283d1194bed1eb3d68f6247
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-05 17:52:25 +0530

    JS

---
+++ JAVASCRIPT/DAY-4/script.js
@@ -0,0 +1,50 @@
+// ARRAYS
+
+// let marks = [97, 82, 75, 64, 36];
+
+// console.log(marks);
+// console.log(marks.length);   //property
+// console.log(typeof(marks));
+
+
+// let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman", "antman"];
+
+// console.log(heroes);
+// console.log(typeof(heroes));
+// console.log(heroes.length);
+
+
+// let marks = [97, 82, 75, 64, 36];
+
+// console.log(marks[0]);
+// console.log(marks[5]);
+
+// marks[2]=76;
+
+// console.log(marks);
+
+
+// let heroes = ["ironman", "thor", "hulk", "shaktiman", "spiderman", "antman"];
+
+//for loop
+// for (let i = 0; i < heroes.length; i++) {
+//     console.log(heroes[idx])
+// }
+
+//for-of loop
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+
+
+// let cities = ["delhi", "pune", "mumbai", "hyderabad", "gurgaon"];
+
+// for (let city of cities) {
+//     console.log(city);
+// }
+
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+// }
+
+
