3211f729caa6056fd0e4ca6e414341aa945430cb
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-05 13:05:06 +0530

    JS

--- JAVASCRIPT/DAY-3/script.js
+++ JAVASCRIPT/DAY-3/script.js
@@ -1,3 +1,53 @@
-for (let i = 1; i <= 5; i++) {
-    console.log("dishita");
+//for loop
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("dishita");
+// }
+
+
+// Calculate sum of first n numbers
+// let sum = 0;
+// n = prompt("Enter a number")
+// for (let i = 0; i <= n; i++) {
+//     sum += i;
+// }
+// console.log("sum of ",n," is ", sum);
+
+
+// for (let i = 1; i <= 5; i++) {
+//  console.log("i = ", i)
+// }
+
+
+
+// while loop
+
+// let i = 1;
+// while (i <= 5) {
+//     console.log("dishita");
+//     i++
+// }
+
+
+
+// do-while loop
+
+// let i = 1;
+// do {
+//     console.log("dishita");
+//     i++;
+// } while (i <= 5);
+
+
+
+//for-of loop
+
+let str = "Dishita";
+
+let size = 0;
+for (let i of str) {
+    console.log("i=", i);
+    size++;
 }
+
+console.log("string size =", size);
