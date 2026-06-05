9a70674a451403fc1833d5efb8f0b96cf87f9cf4
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-05 10:40:03 +0530

    JS

--- JAVASCRIPT/DAY-2/script.js
+++ JAVASCRIPT/DAY-2/script.js
@@ -155,3 +155,27 @@
 // } else {
 //     console.log("odd")
 // }
+
+
+// let mode = "blue";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode === "blue") {
+//     color = "blue";
+// } else if (mode === "pink") {
+//     color = "pink";
+// } else {
+//     color = "white"
+// }
+
+// console.log(color);
+
+
+
+// Ternary Operators
+
+// let age = 18;
+
+// console.log(age >=18 ? "adult" : "not adult");
