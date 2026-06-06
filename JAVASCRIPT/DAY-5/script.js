2779a0b6cc620b510afb3ca0ea813c808ba3b9b3
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-06 18:15:18 +0530

    JS

--- JAVASCRIPT/DAY-5/script.js
+++ JAVASCRIPT/DAY-5/script.js
@@ -138,3 +138,16 @@
 // arr.forEach((val) => {
 //     console.log(val ** 2);
 // });
+
+
+let nums = [67, 52, 39];
+
+// nums.map((val) => {
+//     console.log(val);
+// });
+
+let newArr = nums.map((val) => {
+    return val;
+});
+
+console.log(newArr);
