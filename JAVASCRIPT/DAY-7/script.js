b2dd6d0bf2b67182b894c5c13a62def6718282ce
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-09 00:11:01 +0530

    JS

--- JAVASCRIPT/DAY-7/script.js
+++ JAVASCRIPT/DAY-7/script.js
@@ -45,3 +45,46 @@
 
 // let p = document.querySelector("p");
 // p.after(newBtn);
+
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>New Heading</i>";
+// console.log(newHeading);
+
+// let body = document.querySelector("body");
+
+// body.prepend(newHeading);
+
+// let para = document.querySelector("p");
+// para.remove();
+
+
+
+
+
+// PRACTICE
+
+
+// 1.
+
+// let button = document.createElement("button");
+// button.innerHTML = "Click Me!";
+
+// button.style.backgroundColor = "red";
+// button.style.color = "white";
+
+// let body = document.querySelector("body");
+// body.prepend(button);
+
+
+// 2.
+
+let para = document.querySelector(".para");
+console.log(para);
+
+
+// para.setAttribute("class", "newClass");    // not a good method to add new class because this completely vanish the existing class
+
+// para.classList.add("newClass");
+
+// para.classList.remove("newClass");
