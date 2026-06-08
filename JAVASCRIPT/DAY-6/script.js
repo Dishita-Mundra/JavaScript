37251fb65fc57e8f0881fb46614430120727402b
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-08 22:27:03 +0530

    JS

--- JAVASCRIPT/DAY-6/script.js
+++ JAVASCRIPT/DAY-6/script.js
@@ -79,6 +79,11 @@
 
 // h2.innerText = h2.innerText + " from Apna College";    //concatenate
 
-let divs = document.querySelector(".box");
-console.dir(divs);
+let divs = document.querySelectorAll(".box");
+// console.log(divs);
 
+
+
+// divs[0].innerText = "new unique value 1";
+// divs[1].innerText = "new unique value 2";
+// divs[2].innerText = "new unique value 3";
