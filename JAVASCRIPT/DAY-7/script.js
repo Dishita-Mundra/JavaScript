3b601f45681fef996fea2cde2e962fc84c1a1e32
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-09 00:11:34 +0530

    JS

--- JAVASCRIPT/DAY-7/script.js
+++ JAVASCRIPT/DAY-7/script.js
@@ -79,12 +79,12 @@
 
 // 2.
 
-// let para = document.querySelector(".para");
-// console.log(para);
+let para = document.querySelector(".para");
+console.log(para);
 
 
 // para.setAttribute("class", "newClass");    // not a good method to add new class because this completely vanish the existing class
 
-// para.classList.add("newClass");
+para.classList.add("newClass");
 
 // para.classList.remove("newClass");
