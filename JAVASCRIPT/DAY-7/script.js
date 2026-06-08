f6d1ebdc182dc8fa020c53c7c9c6e8a39da42dee
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-08 23:03:13 +0530

    JS

--- JAVASCRIPT/DAY-7/script.js
+++ JAVASCRIPT/DAY-7/script.js
@@ -1,2 +1,17 @@
-let div = document.querySelector("div");
-console.log(div);
+// let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+
+// let para = document.querySelector("p");
+
+// console.log(para.getAttribute("class"));
+
+// console.log(para.setAttribute("class","paragraph"));
+
+
