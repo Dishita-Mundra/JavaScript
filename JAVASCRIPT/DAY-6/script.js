c7dac09fecd9df9490dfc8930267aaa87b0f4517
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-08 12:37:33 +0530

    JS

--- JAVASCRIPT/DAY-6/script.js
+++ JAVASCRIPT/DAY-6/script.js
@@ -1,2 +1,14 @@
 // if we want any change dynamically on basis of user input we can write this type of code in console window in browser(DOM)
-// document.body.childNodes[3].innerText="abcd";
+// document.body.childNodes[3].innerText = "abcd";
+
+// let header = document.getElementById("heading1");
+
+// console.log(header);
+// console.dir(header);
+
+let button = document.getElementById("myId");
+console.dir(button);
+
+let headings = document.getElementsByClassName("myClass");
+console.log(headings);
+console.dir(headings);
