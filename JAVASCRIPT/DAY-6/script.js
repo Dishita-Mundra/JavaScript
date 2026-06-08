6a8fafe4280d92c55eb4b9b5fbabec7170853372
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-08 19:49:39 +0530

    JS

--- JAVASCRIPT/DAY-6/script.js
+++ JAVASCRIPT/DAY-6/script.js
@@ -1,3 +1,5 @@
+// 1.
+
 // if we want any change dynamically on basis of user input we can write this type of code in console window in browser(DOM)
 // document.body.childNodes[3].innerText = "abcd";
 
@@ -17,3 +19,32 @@
 
 // let para = document.getElementsByTagName("p");
 // console.dir(para);
+
+
+// let firstEl = document.querySelector("p");  //1st element
+// console.dir(firstEl);
+
+
+// let allEl = document.querySelectorAll("p");   // all elements
+// console.dir(allEl);
+
+
+// let firstEl = document.querySelector(".myClass");  //1st element
+// console.dir(firstEl);
+
+
+// let allEl = document.querySelectorAll(".myClass");   // all elements
+// console.dir(allEl);
+
+
+// let firstEl = document.querySelector("#myId");  //1st element
+// console.dir(firstEl);
+
+
+// let allEl = document.querySelectorAll("#myId");   // all elements
+// console.dir(allEl);
+
+
+
+// 2.
+
