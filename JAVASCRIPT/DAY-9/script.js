5bdeec7db79fc3db4a79b360195e54df2384092b
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-09 23:02:45 +0530

    JS

--- JAVASCRIPT/DAY-9/script.js
+++ JAVASCRIPT/DAY-9/script.js
@@ -39,9 +39,15 @@ class ToyotaCar {
     start() {
         console.log("start");
     }
+
     stop() {
         console.log("stop");
+    }
+
+    setBrand(brand) {
+        this.brandName = brand;
     }
 }
 
 let fortuner = new ToyotaCar();
+fortuner.setBrand()
