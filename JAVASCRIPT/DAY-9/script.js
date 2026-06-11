3dd0b95a7201af49299fc0a4c30fb7b19674e08e
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-11 13:07:59 +0530

    JS

--- JAVASCRIPT/DAY-9/script.js
+++ JAVASCRIPT/DAY-9/script.js
@@ -144,28 +144,29 @@
 
 
 
-class Person {
-    constructor(name) {
-        this.species = "homo sapiens"
-        this.name = name;
-    }
+// class Person {
+//     constructor(name) {
+//         this.species = "homo sapiens"
+//         this.name = name;
+//     }
 
-    eat() {
-        console.log("eat"); 
-    }
-}
+//     eat() {
+//         console.log("eat"); 
+//     }
+// }
 
-class Engineer extends Person{
-    constructor(name) {
-        console.log("enter child constructor");
-        super();   // to invoke parent class constructor
-    }
+// class Engineer extends Person{
+//     constructor(name) {
+//         super(name);   // to invoke parent class constructor
+//     }
 
-    work() {
-        console.log("solve problems, build something");
-    }
-}
+//     work() {
+//         super.eat();
+//         console.log("solve problems, build something");
+//     }
+// }
 
-let engObj = new Engineer("dishita");
+// let engObj = new Engineer("dishita");
 
-console.log(engObj);
+// console.log(engObj);
+// console.log(engObj.work());
