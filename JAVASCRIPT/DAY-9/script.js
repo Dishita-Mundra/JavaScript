29c550439714c659454ddd242a43aa7e3f165e3f
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-11 13:50:16 +0530

    JS

--- JAVASCRIPT/DAY-9/script.js
+++ JAVASCRIPT/DAY-9/script.js
@@ -213,21 +213,21 @@
 
 // ERROR HANDLING
 
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-console.log("a= ", a);
-console.log("b= ", b);
-console.log("a + b ", a + b);
+// console.log("a= ", a);
+// console.log("b= ", b);
+// console.log("a + b ", a + b);
 
-try{
-    console.log("a + b ", a + c);
-} catch(err) {
-    console.log(err);
-}
+// try{
+//     console.log("a + b ", a + c);
+// } catch(err) {
+//     console.log(err);
+// }
 
-console.log("a + b ", a + b);
-console.log("a + b ", a + b);
-console.log("a + b ", a + b);
-console.log("a + b ", a + b);
-console.log("a + b ", a + b);
+// console.log("a + b ", a + b);
+// console.log("a + b ", a + b);
+// console.log("a + b ", a + b);
+// console.log("a + b ", a + b);
+// console.log("a + b ", a + b);
