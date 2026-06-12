350e34f863af22a9ec303b6fbbc8306888d40f07
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-12 22:19:25 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -7,3 +7,33 @@
 
 // console.log("three");
 // console.log("four");
+
+
+
+// function sum(a, b){
+//     console.log(a + b);
+// }
+
+// function calculator(a, b, sumCallback) {
+//     sumCallback(a, b);
+// }
+
+// calculator(1, 2, sum);
+
+// OR
+
+// calculator(1, 2, (a, b) => {
+//     console.log(a + b);
+// });
+
+
+
+// const hello = () => {
+//     console.log("hello");
+// }
+
+// setTimeout(hello, 3000);
+
+
+
+// nesting
