a30a8b97469ebe7675d5b19f4c688d5259e181c7
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-12 22:21:47 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -38,13 +38,13 @@
 
 // nesting
 
-let age = 19;
-if (age>=18) {
-    if (age >=60) {
-        console.log("senior");        
-    } else {
-        console.log("middle");
-    }
-} else {
-    console.log("child");
-}
+// let age = 19;
+// if (age>=18) {
+//     if (age >=60) {
+//         console.log("senior");        
+//     } else {
+//         console.log("middle");
+//     }
+// } else {
+//     console.log("child");
+// }
