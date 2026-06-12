cfa3d72d858a7ce3b863581d37ce987c067c767a
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-12 22:29:16 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -50,10 +50,14 @@
 // }
 
 
-for (let i = 0; i < 5; i++) {
-    let str = "";
-    for (let j = 0; j < 5; j++) {
-        str = str + j;
-    }
-    console.log(str);
-}
+// for (let i = 0; i < 5; i++) {
+//     let str = "";
+//     for (let j = 0; j < 5; j++) {
+//         str = str + j;
+//     }
+//     console.log(str);
+// } 
+
+
+
+
