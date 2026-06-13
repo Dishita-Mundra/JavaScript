5d4ee72b427005af5b7ce07961c3fd6deb7dc712
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-13 12:24:53 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -70,17 +70,23 @@
 // console.log(getData(1));
 
 
-function getData(dataId, getNextData) {
-    setTimeout(() => {
-        console.log("data", dataId);
-        if (getNextData){
-            getNextData();
-        }
-    }, 2000);
-}
+// function getData(dataId, getNextData) {
+//     setTimeout(() => {
+//         console.log("data", dataId);
+//         if (getNextData){
+//             getNextData();
+//         }
+//     }, 2000);
+// }
 
-getData(1, () => {
-    getData(2, () => {
-        getData(3);
-    });
-});
+// // callback hell
+// getData(1, () => {
+//     console.log("getting data2 ....");
+//     getData(2, () => {
+//         console.log("getting data3 ....");
+//         getData(3, () => {
+//             console.log("getting data4 ....");
+//             getData(4);
+//         });
+//     });
+// });
