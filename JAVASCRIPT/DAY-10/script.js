b78b340e1a2eeaef4f5d904798337b47350d3503
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-14 18:43:22 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -102,17 +102,17 @@
 //     reject("some error occured");
 // });
 
-function getData(dataId, getNextData) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        // console.log("data", dataId);
-        // resolve("success");
-        reject("error");
-        if (getNextData){
-            getNextData();
-        }
-    }, 5000);
-    })
-}
+// function getData(dataId, getNextData) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         // console.log("data", dataId);
+//         // resolve("success");
+//         reject("error");
+//         if (getNextData){
+//             getNextData();
+//         }
+//     }, 5000);
+//     })
+// }
 
-let promise = getData(123);
+// let promise = getData(123);
