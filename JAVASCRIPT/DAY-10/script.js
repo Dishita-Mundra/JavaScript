0c2aea69f2f23458d1196effd2d41105c39fc638
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-14 18:48:19 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -116,3 +116,16 @@
 // }
 
 // let promise = getData(123);
+
+
+const getPromise = () => {
+    new Promise((resolve, reject) => {
+        console.log("I am a promise");
+        resolve("123"); 
+    });
+};
+
+let promise = getPromise();
+promise.then(() => {
+    console.log("promise fulfilled");
+});
