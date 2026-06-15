e8ab5a07bec0ef4f98f5d17c091bbe1a16606e46
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-15 22:51:49 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -71,12 +71,12 @@
 
 
 // function getData(dataId, getNextData) {
-    // setTimeout(() => {
-    //     console.log("data", dataId);
-    //     if (getNextData){
-    //         getNextData();
-    //     }
-    // }, 2000);
+// setTimeout(() => {
+//     console.log("data", dataId);
+//     if (getNextData){
+//         getNextData();
+//     }
+// }, 2000);
 // }
 
 // // callback hell
@@ -172,17 +172,28 @@
 function getData(dataId, getNextData) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-        console.log("data", dataId);
-        resolve("success");
-        // reject("error");
-        if (getNextData){
-            getNextData();
-        }
-    }, 5000);
+            console.log("data", dataId);
+            resolve("success");
+            // reject("error");
+            if (getNextData) {
+                getNextData();
+            }
+        }, 2000);
     })
 }
 
 // PROMISE CHAIN
-getData(1).then((res) => {
-    console.log(res);
-});
+
+console.log("getting data1 ....");
+getData(1)
+   .then((res) => {
+      console.log("getting data2 ....");
+      return getData(2);
+   })
+   .then((res) => {
+      console.log("getting data3 ....");
+      return getData(3);
+   })
+   .then((res) => {
+      console.log(res);
+   });
