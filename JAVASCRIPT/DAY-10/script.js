a2d3d18e46cb189cc3a0d0842f057755dfd78a7b
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-15 22:32:51 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -137,3 +137,32 @@
 
 
 
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data1");
+//             resolve("success");
+//         }, 4000);
+//     });
+// }
+
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data2");
+//             resolve("success");
+//         }, 4000);
+//     });
+// }
+
+// console.log("fetching data1....");
+// asyncFunc1().then((res) => {
+//     console.log("fetching data2....");
+//     asyncFunc2().then((res) => {});
+// });
+
+// console.log("fetching data2....");
+// let p2 = asyncFunc2();
+// p2.then((res) => {
+//     console.log(res);
+// });
