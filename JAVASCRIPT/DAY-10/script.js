32f5534970ac7f771122b5e7ad48ef92377bb254
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-16 16:56:14 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -199,3 +199,25 @@
 //    });
 
 
+
+// ASYNC/AWAIT
+
+// async function hello() {
+//     console.log("hello");
+// }
+
+
+function api() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("weather data");
+            resolve(200);
+        }, 2000);
+    });
+}
+
+async function getWeatherdata() {
+    await api();  //1st
+    await api();  //2nd
+}
+
