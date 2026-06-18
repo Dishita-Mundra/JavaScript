ba7fcd97f0d26d9f22440c6a408640a46a28ee16
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-18 18:38:51 +0530

    JS

--- JAVASCRIPT/DAY-11/script.js
+++ JAVASCRIPT/DAY-11/script.js
@@ -1,9 +1,15 @@
-const URL = "https://dogapi.dog/docs/api-v2";
+const URL = "https://jsonplaceholder.typicode.com/todos/1";
+const factPara = document.querySelector("#fact");
 
 let promise = fetch(URL);
 console.log(promise);
 
 const getFacts = async () => {
-    let promise = await fetch(URL);
-    console.log(promise);
+    console.log("getting data....");
+    let response = await fetch(URL);
+    console.log(response);   //JSON format
+    let data = await response.json();
+    console.log(data);
 }
+
+console.log(getFacts());
