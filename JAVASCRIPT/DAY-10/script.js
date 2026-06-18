50e2dc01b9c03b724db23ee2a939e054e892b0d6
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-18 15:57:12 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -228,14 +228,14 @@
 
 // IIFE (IMMEDIATELY INVOKED FUNCTION EXPRESSION)
 
-(async function getAllData() {
-    console.log("getting data1....");
-    await getData(1);
-    console.log("getting data2....");
-    await getData(2);
-    console.log("getting data3....");
-    await getData(3);
-})();
+// (async function getAllData() {
+//     console.log("getting data1....");
+//     await getData(1);
+//     console.log("getting data2....");
+//     await getData(2);
+//     console.log("getting data3....");
+//     await getData(3);
+// })();
 
 
 // async function getWeatherdata() {
