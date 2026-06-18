19a59020a086e3c72dc3038934a571d0e3a61093
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-18 15:50:43 +0530

    JS

--- JAVASCRIPT/DAY-10/script.js
+++ JAVASCRIPT/DAY-10/script.js
@@ -226,6 +226,17 @@
 // }
 
 
+// IIFE (IMMEDIATELY INVOKED FUNCTION EXPRESSION)
+
+(async function getAllData() {
+    console.log("getting data1....");
+    await getData(1);
+    console.log("getting data2....");
+    await getData(2);
+    console.log("getting data3....");
+    await getData(3);
+})();
+
 
 // async function getWeatherdata() {
 //     await api();  //1st
